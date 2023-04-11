d7219ccddc58ac111c78066355e3eb2ddebd2b60
unknown <narmada.vaddireddy97@gmail.com>
2023-04-11 19:21:51 +0530

    Added: Code to print the digits that are repeated twice

--- ArrayPracticeProblems.js
+++ ArrayPracticeProblems.js
@@ -1,15 +1,8 @@
-//Program to show Sum of three Integer adds to ZERO
-let a=[-1,-2,1,2,-3];
-let b=5;
-
-for (i = 0; i < a.length; i++) {
-    for (j = i + 1; j < a.length; j++) {
-        for (l = j + 1; l < a.length; l++) {
-            if (a[i] + a[j] + a[l] == 0) {
-                console.log("Found 3 elements whose sum is = 0");
-               console.log("Elements are " + a[i] + " " + a[j] + " " + a[l]);
-                return;
-            }
-        }
+//range from 0 – 100, find the digits that are repeated twice like 33, 77, etc and store them in an array
+let repeatedNumbers = [];
+for(i = 1; i <= 100; i++){
+    if(i % 11 == 0){
+        repeatedNumbers.push(i);
     }
 }
+console.log("Repeated numbers from 1 to 100 are :"+ arrayOfRepeatedNumbers);
