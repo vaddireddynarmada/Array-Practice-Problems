97400f174de5d75df7a4d280c0145e3221074e55
unknown <narmada.vaddireddy97@gmail.com>
2023-04-11 18:55:08 +0530

    Added: Code for Prime Factorization

--- ArrayPracticeProblems.js
+++ ArrayPracticeProblems.js
@@ -1,11 +1,27 @@
-//sort the array and then find the 2nd largest and the 2nd smallest element.
+//Prime Factorization Program to store all the Prime Factors of a number n into an array
+// and finally display the output
 
-let arrayOfRandomNumbers = [];
-for(i = 0; i <= 10; i++){
-    randomNumber = Math.floor(Math.random() * 899 + 100);
-    arrayOfRandomNumbers.push(randomNumber);
+
+
+let arrayOfPrimeFactors = [];
+let number = 34;
+
+for (i = 2; i <= number; i++) {
+    let a = number % i;
+    if (a == 0) {
+        isPrime = 1;
+        for (j = 2; j <= i / 2; j++) {
+            let b = i % j;
+            if (b == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime == 1) {
+            arrayOfPrimeFactors.push(i);
+        }
+    }
 }
-arrayOfRandomNumbers.sort();
-console.log("Random Numbers : " +arrayOfRandomNumbers);
-console.log("Second largest element is " + arrayOfRandomNumbers[9]);
-console.log("Second smallest element is " + arrayOfRandomNumbers[1]);
+if (isPrime == 0 || isPrime == 1) {
+    console.log(arrayOfPrimeFactors);
+}
